c48e122d8f483545f59edc65ce73d8b59ca17833
ian <iandominoniti@gmail.com>
2026-03-10 17:07:28 -0300

    feat: busca por episodios p/ temporada

--- src/main/java/br/com/alura/screenmatch/main/MainFunc.java
+++ src/main/java/br/com/alura/screenmatch/main/MainFunc.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ApiCallOmdb;
@@ -14,29 +15,35 @@ public class MainFunc {
     ApiCallOmdb api = new ApiCallOmdb();
 
     public void menu() throws IOException, Exception {
-        System.out.println("Digite o nome da série para buscar: ");
-        String resposta = leitura.nextLine();
-        paramsSerie.put("t", resposta);
-        var responseSerie = api.get(paramsSerie);
-//		Map<String,String> paramsEpisodio = new HashMap();
-//		paramsEpisodio.put("t", "Gilmore Girls");
-//		paramsEpisodio.put("Season", "1");
-//		paramsEpisodio.put("Episode","1");
-//		var responseEpisodio = ApiCall.get(paramsEpisodio);
-//		DadosEpisodio dadosEpisodio = conversor.obterDados(responseEpisodio, DadosEpisodio.class);
-//		System.out.println(dadosEpisodio);
-        ConverterDados conversor = new ConverterDados();
-        DadosSerie dadosSerie = conversor.obterDados(responseSerie, DadosSerie.class);
+        try {
+            System.out.println("Digite o nome da série para buscar: ");
+            String resposta = leitura.nextLine();
+            paramsSerie.put("t", resposta);
+            var responseSerie = api.get(paramsSerie);
 
-        List<DadosTemporada> temporadas = new ArrayList<>();
-        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-            Map<String,String> paramsTemporadas = new HashMap();
-            paramsTemporadas.put("t", resposta);
-            paramsTemporadas.put("Season", String.valueOf(i));
-            var responseTemporada = api.get(paramsTemporadas);
-            DadosTemporada dadosTemporada = conversor.obterDados(responseTemporada, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
-        }
-        temporadas.forEach(System.out::println);
+            ConverterDados conversor = new ConverterDados();
+            DadosSerie dadosSerie = conversor.obterDados(responseSerie, DadosSerie.class);
+
+            List<DadosTemporada> temporadas = new ArrayList<>();
+            for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+                Map<String,String> paramsTemporadas = new HashMap();
+                paramsTemporadas.put("t", resposta);
+                paramsTemporadas.put("Season", String.valueOf(i));
+                var responseTemporada = api.get(paramsTemporadas);
+                DadosTemporada dadosTemporada = conversor.obterDados(responseTemporada, DadosTemporada.class);
+                temporadas.add(dadosTemporada);
+            }
+            for (int i = 0;i <dadosSerie.totalTemporadas() ; i++) {
+                List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+                System.out.printf("Temporada: %d%n", i+1);
+                for(int j =0; j< episodiosTemporada.size(); j++){
+                    System.out.println(episodiosTemporada.get(j).titulo());
+                }
+        }}
+        catch(Exception e){
+                System.out.println("Erro " + e);
+                menu();
+            }
+
     }
 }
